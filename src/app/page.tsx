153df828c986cb6570c9cd9f83c33af3e7306848
Artem Klochkov <Arthorror@gmail.com>
2026-03-16 22:34:53 +0400

    setup trpc (#2)
    Co-authored-by: Artem Klochkov <aklochkov@atypicaldigital.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,47 @@
 import {cn} from "@/lib/utils";
-import prisma from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import {Client} from "@/app/client";
+import {dehydrate, HydrationBoundary} from "@tanstack/react-query";
+import {Suspense} from "react";
 
 const Page = async () => {
-    const users = await prisma.user.findMany();
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
 
   const smth = true;
 
   return (
     <div className={cn("text-red-500", {'mt-2': smth})}>
-        {JSON.stringify(users)}
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Client/>
+            </Suspense>
+        </HydrationBoundary>
+
     </div>
   )
 
 };
 
 export default Page;
+
+// "use client"
+//
+// import {useTRPC} from "@/trpc/client";
+// import {useQuery} from "@tanstack/react-query";
+//
+// const Page =  () => {
+//     const trpc = useTRPC();
+//     const {data: users} = useQuery(trpc.getUsers.queryOptions());
+//
+//     return (
+//         <div>
+//             {JSON.stringify(users)}
+//         </div>
+//     )
+//
+// };
+//
+// export default Page;
+
